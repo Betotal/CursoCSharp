b067031259558348598e5b214df333e9f8624a33
Betotal <betotall@gmail.com>
2019-04-17 11:49:33 -0300

    Aula 136 - Fixacao

---
+++ Intermediario/Aula136_Fixacao/Aula136_Fixacao/Entities/PessoaFisica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Aula136_Fixacao.Entities;
+
+namespace Aula136_Fixacao.Entities
+{
+    class PessoaFisica : Pessoa
+    {
+        public double GastoSaude { get; set; }
+
+        public PessoaFisica(string nome, double rendaAnual, double gastoSaude) : base(nome, rendaAnual)
+        {
+            GastoSaude = gastoSaude;
+            Imposto = CalcImposto();
+        }
+
+        public override double CalcImposto()
+        {
+            return (RendaAnual < 20000) ? ((RendaAnual * 15 / 100) - (GastoSaude/2)): ((RendaAnual * 25 / 100) - (GastoSaude / 2));
+        }
+    }
+}
