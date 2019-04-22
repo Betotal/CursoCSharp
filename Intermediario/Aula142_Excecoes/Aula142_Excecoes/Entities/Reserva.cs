571ab6bc76bc76ade7ffcf42102d15eb695789a5
Betotal <betotall@gmail.com>
2019-04-22 13:07:44 -0300

    Aula 142 - Exemplo Ruim

---
+++ Intermediario/Aula142_Excecoes/Aula142_Excecoes/Entities/Reserva.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Aula142_Excecoes.Entities
+{
+    class Reserva
+    {
+        public int NumQuarto { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reserva()
+        {
+        }
+
+        public Reserva(int numQuarto, DateTime checkIn, DateTime checkOut)
+        {
+            NumQuarto = numQuarto;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duracao()
+        {
+            TimeSpan duracao = CheckOut.Subtract(CheckIn);
+            return (int)duracao.TotalDays;
+
+        }
+
+        public void UpdateReserva(DateTime checkin, DateTime checkout)
+        {
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Numero do Quarto: "
+                 + NumQuarto
+                 + ", check-in: "
+                 + CheckIn.ToString("dd/MM/yyyy")
+                 + ", check-out: "
+                 + CheckOut.ToString("dd/MM/yyyy")
+                 + ", "
+                 + Duracao()
+                 + " noites.";
+        }
+    }
+}
---
+++ Intermediario/Aula142_Excecoes/Aula142_Excecoes/Program.cs
@@ -0,0 +1,73 @@
+﻿using Aula142_Excecoes.Entities;
+using System;
+
+namespace Aula142_Excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****     USO DE EXCEÇÕES     *****");
+            Console.WriteLine();
+            Console.Write("Informe o número do Quarto: ");
+            int quarto = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.Write("Data de entrada (dd/mm/aaaa - Check-in): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Data de saida (dd/mm/aaaa - Check-out): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+            DateTime now = DateTime.Now;
+            if (now > checkin || now > checkout)
+            { 
+                Console.WriteLine("Reserva só é permitida para datas futuras");
+            }
+            else {
+                if (checkout <= checkin)
+                {
+                    Console.WriteLine("Data de Check-out deve ser maior que a data de entrada");
+                }
+                else
+                {
+                    Reserva reserva = new Reserva(quarto, checkin, checkout);
+                    Console.WriteLine($"Reserva: " + reserva);
+                    Console.WriteLine();
+
+                    Console.WriteLine("Atualização de reserva");
+                    Console.Write("Data de entrada (dd/mm/aaaa - Check-in): ");
+                    checkin = DateTime.Parse(Console.ReadLine());
+
+                    Console.Write("Data de saida (dd/mm/aaaa - Check-out): ");
+                    checkout = DateTime.Parse(Console.ReadLine());
+
+                    now = DateTime.Now;
+                    if (now > checkin || now > checkout)
+                    {
+                        Console.WriteLine("Reserva só é permitida para datas futuras");
+                    }
+                    else
+                    {
+                        if (checkout <= checkin)
+                        {
+                            Console.WriteLine("Data de Check-out deve ser maior que a data de entrada");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Dados atualizados");
+                            reserva.UpdateReserva(checkin, checkout);
+                            Console.WriteLine($"Reserva: " + reserva);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
