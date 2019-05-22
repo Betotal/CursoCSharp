7d5ad87d5507d1dd9ee731c340c2f95a8e03d98e
Betotal <betotal@gmail.com>
2019-05-22 00:16:20 -0300

    Arquivos FileStream StreamReader StreamWriter

---
+++ Arquivo/Aula183/Aula183/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Aula183 {
+    class Program {
+        static void Main(string[] args) {
+            string arqPath = @"D:\VisualStudio\MyFiles\File1.txt";
+
+            try {
+                //   aula 187
+                //     var Folders = Directory.EnumerateDirectories(arqPath, "*.*", SearchOption.AllDirectories);
+                //     Console.WriteLine("Pastas:");
+                //     foreach(string f in Folders) {
+                //         Console.WriteLine(f);
+                //     }
+                //     
+                //     var Files = Directory.EnumerateFiles(arqPath, "*.*", SearchOption.AllDirectories);
+                //     Console.WriteLine("Arquivos:");
+                //     foreach (string f in Files) {
+                //         Console.WriteLine(f);
+                //     }
+                //     
+                //     Directory.CreateDirectory(arqPath + @"\Teste");
+                Console.WriteLine("" + Path.DirectorySeparatorChar);
+                Console.WriteLine("" + Path.PathSeparator);
+                Console.WriteLine("" + Path.GetDirectoryName(arqPath));
+                Console.WriteLine("" + Path.GetFileName(arqPath));
+                Console.WriteLine("" + Path.GetExtension(arqPath));
+                Console.WriteLine("" + Path.GetFileNameWithoutExtension(arqPath));
+                Console.WriteLine("" + Path.GetFullPath(arqPath));
+                Console.WriteLine("" + Path.GetTempPath());
+
+
+
+
+
+
+            } catch (IOException e) {
+                Console.Write("Algum erro ocorreu: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Arquivo/Exer_Fixacao_189/Exer_Fixacao_189/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO
+
+namespace Exer_Fixacao_189 {
+    class Program {
+        static void Main(string[] args) {
+            string fileData = @"D:\VisualStudio\MyFiles\Entrada.csv";
+            string Diretorio = Path.GetDirectoryName(fileData);
+            string DiretorioOut = Directory.CreateDirectory(Diretorio + @"\out").ToString();
+            string outFile = File.Create(DiretorioOut + @"\Summary.csv").ToString();
+
+            try {
+                using (StreamReader SR = File.OpenText(fileData)) {
+                    using (StreamWriter SW = File.AppendText(outFile)) {
+                        while (!SR.EndOfStream) {
+                            string[] line = SR.ReadLine().Split(",");
+
+                            string Produto = line[01];
+                            double Preco = double.Parse(line[02]);
+                            int qtd = int.Parse(line[03]);
+
+                            double precoOut = Preco * qtd;
+
+                            SW.WriteLine(Produto + "," + precoOut.ToString());
+                        }
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.Write("Erro: " + e.Message);
+            }
+
+            using
+            
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
