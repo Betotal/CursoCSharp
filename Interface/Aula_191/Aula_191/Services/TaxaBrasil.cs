2285c793d202431ed31e14ccc7ce94ad326df00e
Betotal <betotall@gmail.com>
2020-02-26 12:49:22 -0300

    atualizacao

--- Interface/Aula_191/Aula_191/Services/TaxaBrasil.cs
+++ Interface/Aula_191/Aula_191/Services/TaxaBrasil.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace Aula_191.Services {
     class TaxaBrasil : ITaxaServico {
         public double Taxa(double quantia) {
---
+++ Interface/Aula_196/Aula_196/Entities/Contrato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula_196.Entities
+{
+    class Contrato
+    {
+        public int numContrato { get; set; }
+        public DateTime dtContrato { get; set; }
+        public double valContrato { get; set; }
+        public List<Parcela> Parcelas { get; set; }
+
+        public Contrato(int numContrato, DateTime dtContrato, double valContrato)
+        {
+            this.numContrato = numContrato;
+            this.dtContrato = dtContrato;
+            this.valContrato = valContrato;
+            Parcelas = new List<Parcela>();
+         }
+
+        public void AddParcela(Parcela Parcela)
+        {
+            Parcelas.Add(Parcela);
+        }
+    }
+}
